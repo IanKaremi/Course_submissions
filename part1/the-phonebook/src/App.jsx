34331947118d4159650c5d0f7d03c18c383cc030
IanKaremi <karemiianofficial@gmail.com>
2025-08-13 12:31:58 +0300

    exercise 2.11

--- part1/the-phonebook/src/App.jsx
+++ part1/the-phonebook/src/App.jsx
@@ -1,51 +1,52 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import Filter from './components/filter'
 import PersonForm from './components/personForm'
 import Persons from './components/persons'
+import axios from 'axios'
 
 const App = () => {
 
-  const [persons, setPersons] = useState([
-    { name: 'Arto Hellas', number: '040-123456', id: 1 },
-    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
-    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
-    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
-  ]) 
+  
+  const get_data = () =>{
+    axios.get("http://localhost:3001/persons").then(
+      response => {
+        const person_data = response.data
+        console.log(person_data);
+        setPersons(person_data)
+      
+        
+      }
+    )
+  }
+  useEffect(get_data,[])
+
+  const [persons, setPersons] = useState([]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
   const [param, setParam] = useState('')
   const [res, setRes] = useState([])
 
   function handleNameChange(e){
-  
     setNewName(e.target.value)
-    
-    
   }
+
   const handlePhoneChange = (y) =>{
     setNewPhone(y.target.value)
     console.log(newPhone);
-    
-
   }
 
   const handleSubmit = (v) =>{
     v.preventDefault()
-    
     let val = persons.find(obj => obj.name === newName)
     if(val === undefined){
       setPersons(prev => prev.concat({name: newName, number:newPhone}))
     }else{
       alert(`${newName} already exists.`)
     }
-    
-    
   }
 
   const renderPerson = persons.map((x)=>{return <li key={x.id}>{x.name} - {x.number}</li>})
-
   const renderRes = res.map((x)=>{return <li key={x.id}>{x.name} - {x.number}</li>})
-
 
   const handleParamChange = (y) =>{
     setParam(y.target.value)
@@ -57,11 +58,8 @@ const App = () => {
         temp_arr.push(x) 
       }
     })
-    
     //console.log('results',temp_arr)
     setRes(temp_arr)
-    
-
   }
  
 
