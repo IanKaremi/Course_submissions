4c48c6314e8d6f7bc1433f7107adacab106100fb
IanKaremi <karemiianofficial@gmail.com>
2025-08-13 11:32:25 +0300

    exercise 2.9

--- part1/the-phonebook/src/App.jsx
+++ part1/the-phonebook/src/App.jsx
@@ -3,10 +3,15 @@ import { useState } from 'react'
 const App = () => {
 
   const [persons, setPersons] = useState([
-    { name: 'Arto Hellas' }
+    { name: 'Arto Hellas', number: '040-123456', id: 1 },
+    { name: 'Ada Lovelace', number: '39-44-5323523', id: 2 },
+    { name: 'Dan Abramov', number: '12-43-234345', id: 3 },
+    { name: 'Mary Poppendieck', number: '39-23-6423122', id: 4 }
   ]) 
   const [newName, setNewName] = useState('')
   const [newPhone, setNewPhone] = useState('')
+  const [param, setParam] = useState('')
+  const [res, setRes] = useState([])
 
   function handleNameChange(e){
   
@@ -34,12 +39,36 @@ const App = () => {
     
   }
 
-  const renderPerson = persons.map((x)=>{return <li key={x.name}>{x.name} - {x.number}</li>})
+  const renderPerson = persons.map((x)=>{return <li key={x.id}>{x.name} - {x.number}</li>})
+
+  const renderRes = res.map((x)=>{return <li key={x.id}>{x.name} - {x.number}</li>})
+
+
+  const handleParamChange = (y) =>{
+    setParam(y.target.value)
+    let temp_arr = []
+    
+    persons.forEach((x)=>{
+      if (x.name.toLowerCase().includes(y.target.value)){
+        console.log(y.target.value);
+        temp_arr.push(x) 
+      }
+    })
+    
+    console.log('results',temp_arr)
+    setRes(temp_arr)
+    
+
+  }
  
 
   return (
     <div>
-      <h2>Phonebook</h2>
+      <h1>Phonebook</h1>
+      <form>
+        filter by name <input value={param} onChange={handleParamChange} />
+      </form>
+      <h2>add a new</h2>
       <form onSubmit={handleSubmit}>
         <div>
           name: <input value={newName} onChange={handleNameChange}/>
@@ -52,7 +81,7 @@ const App = () => {
         </div>
       </form>
       <h2>Numbers</h2>
-      <ul>{renderPerson}</ul>
+      <ul>{res.length > 0 ? renderRes :renderPerson}</ul>
     </div>
   )
 }
